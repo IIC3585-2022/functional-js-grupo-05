cbcbc4534f3b9f663bf61686b532bbc96e9e603c
Javiera Inostroza Ríos <26410892+javiinostroza@users.noreply.github.com>
2022-03-21 22:30:10 -0300

    Add ingresar_jugada, init_game and get_score functions

--- t1.js
+++ t1.js
@@ -6,9 +6,16 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const ingresar_jugada = () => undefined;
+const ingresar_jugada = (player, score, plays) => {
+    final_score = get_score(score, plays)
+    return [player, final_score]
+}
 
-const init_game = (players) => undefined;
+const init_game = (players) => {
+    // Ejemplo retorno: [[ 'chamo', 501], ['elias', 501], ['javi', 501]]
+    const abuild = (list_, f)=>list_.map(f) 
+    return abuild(players, x=>[x, 501])
+ } ; 
 
 const play_game = (...players) => 
 {
@@ -45,6 +52,23 @@ const game_loop = (players) => {
     winner.length == 1 ? winner[0] : game_loop(players)((turn + 1)% players.length);
   };
 };
+
+const get_score = (pActual, throws) => {
+    // Recibe puntaje del jugador que lanza y sus tres lanzamientos
+    // Retorna el puntaje del jugador luego de los tres lanzamientos
+    // El cálculo se realiza para cada lanzamiento y se retorna el valor del cálculo final.
+    // Si en alguno de los tres lanzamientos el puntaje se hace cero, entonces retorna 0
+    let winner = false;
+    throws.map(x => {
+        x == 'DB'
+            ? pActual = Math.abs(pActual-50)
+            : x == 'SB' 
+                ? pActual = Math.abs(pActual-25)
+                : pActual = Math.abs(pActual - x.reduce((x1, x2) => (x1*x2)))
+        if (pActual == 0){winner = true};
+    })
+    return winner ? 0 : pActual;
+}; 
 
 play_game('Ema', 'Juan');
 //game_loop([]);
