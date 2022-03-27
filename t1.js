fd6a9b4a29e4c138cec23f021660881885b1a379
Javiera Inostroza Ríos <26410892+javiinostroza@users.noreply.github.com>
2022-03-27 18:15:51 -0300

    Add demo

--- t1.js
+++ t1.js
@@ -9,20 +9,24 @@ const init_game = (players) => abuild(players, x => [x, 501]);
 const play_game = async (...players) => {
   const gamers = init_game(players);
   msg_curried("Juego inicializado con jugadores")(...players);
-  const winner = await game_loop(gamers)(0);
+  const winner = await game_loop(gamers, [[[3,20],[3,19],[2,12]],[[3,13],[3,13],[3,13]],[[3,20],[3,20],[3,20]],[[3,12],[3,12],[3,12]],[[3,20],[3,20],[3,20]]])(0);
+
   msg_curried("El juego ha finalizado, un jugador ha llegado a 0 puntos. Felicidades por ganar")(winner[0]);
 };
 
 const msg_curried = (msg) => (...players) => console.log(msg + " " + players.reduce((acc, player) => acc + ", " + player));
 const min_score = (players) => players.filter(player => player[1] == 0);
 
-const game_loop = (players) => {
+const game_loop = (players, allPlays) => {
   return async (turn) => {
-    const plays = await get_input("\nIngrese lanzamientos de " + players[turn][0] + ": ");
-    players[turn] = ingresar_jugada(players[turn][0], players[turn][1], JSON.parse(plays));
+    console.log("\nIngrese lanzamientos de " + players[turn][0] + ": ")
+    const plays = allPlays.pop()
+    await new Promise(r => setTimeout(r,4000));
+    console.log(" ", plays)
+    players[turn] = ingresar_jugada(players[turn][0], players[turn][1], plays);
     const winner = min_score(players);
     print_scores(players)
-    return winner.length == 1 ? winner[0] : game_loop(players)((turn + 1)% players.length);
+    return winner.length == 1 ? winner[0] : game_loop(players, allPlays)((turn + 1)% players.length);
   };
 };
 
@@ -54,8 +58,10 @@ const get_score = (pActual, throws) => {
 const print_scores = (players) => players.forEach(player => console.log("Puntaje de " + player[0] + ": " + player[1]));
 
 async function init_players(){
-  const players = await get_input("Ingrese el nombre de los jugadores: ");
-  play_game(...players.split(','))
+  const players = console.log("Ingrese el nombre de los jugadores: ");
+  await new Promise(r => setTimeout(r,1000));
+
+  play_game('Gary', 'Joe')
 }
 
 init_players()
